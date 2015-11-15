319f50501b12c45f59357760ba441e00632abbcc
Janre <janre.press@khk.ee>
2015-11-15 22:07:29 +0200

    Config database

--- include/database.php
+++ include/database.php
@@ -1,7 +1,6 @@
 <?php
-//connect DB
+//Connect database
 require_once 'config.php';
 $db = mysqli_connect(DATABASE_HOSTNAME, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_DATABASE) or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
-
 ?>
