362d8e2cc3236dda34d2d084e457178481f0d685
Janre <janre.press@khk.ee>
2015-11-15 18:43:24 +0200

    Config database

--- include/database.php
+++ include/database.php
@@ -1,6 +1,7 @@
 <?php
 //connect DB
-$db = mysqli_connect('127.0.0.1', 'root', '', 'cleanbloguus') or die(mysqli_error($db));
+require_once 'config.php';
+$db = mysqli_connect('DATABASE_HOSTNAME', 'DATABASE_USERNAME', 'DATABASE_PASSWORD', 'DATABASE_DATABASE') or die(mysqli_error($db));
 mysqli_query($db, "SET NAMES 'utf8'");
 
 ?>
