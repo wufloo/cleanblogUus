52fa508d4a0b5979aba007c028e539c376c927e6
Janre <janre.press@khk.ee>
2015-11-15 13:59:50 +0200

    Parandatud viga, mille tõttu ei laaditud õiget alamlehte

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $page_load_start = microtime(1);
 require 'include/database.php';
 
 //Set page
-$page = empty($_GET['page']) && file_exists("pages/$page.php") ? $_GET['page'] : 'home';
+$page = !empty($_GET['page']) && file_exists("pages/$_GET[page].php") ? $_GET['page'] : 'home';
 
 //Include required page
 require "templates/master_template.php";
