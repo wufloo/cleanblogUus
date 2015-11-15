319be14a0198e47fb8dcdefc0e5c714006672883
Janre <janre.press@khk.ee>
2015-11-15 22:24:07 +0200

    Config database

--- config.sample.php
+++ config.sample.php
@@ -2,4 +2,4 @@
 define('DATABASE_HOSTNAME', '127.0.0.1');
 define('DATABASE_USERNAME', 'root');
 define('DATABASE_PASSWORD', '');
-define('DATABASE_DATABASE', PROJECT_NAME);
+define('DATABASE_DATABASE', 'cleanblogUus');
--- controllers/post.php
+++ controllers/post.php
@@ -1,11 +1,8 @@
 <?php
-
-//Get post_id
+// Get post id
 $post_id = isset($_GET['id']) ? $_GET['id'] : -1;
-
-
+require 'include/database.php';
 //Retrieve data from DB
 $q = mysqli_query($db, "SELECT *, DATE_FORMAT(post_created, '%d.%m.%Y %H:%i') post_created FROM posts NATURAL JOIN authors WHERE post_id=$post_id");
-
 $post = mysqli_fetch_assoc($q);
 ?>
